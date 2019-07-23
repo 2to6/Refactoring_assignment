2dda0b052e665cf0a6f02afa498d1369f45e535e
ChoeJiwon <jiwon0929@ajou.ac.kr>
2019-07-23 21:05:23 +0900

    other 품목에 sellIn -1 추가

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -26,6 +26,7 @@ class DirtySample {
 
     private void update_other_quality(Item item) {
         if(item.quality > 0) item.quality -= 1;
+        item.sellIn -= 1;
         if(item.sellIn <= 0) item.quality -= 1;
     }
 
