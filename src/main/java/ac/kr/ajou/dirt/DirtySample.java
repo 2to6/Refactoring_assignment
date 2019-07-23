0049954a1e131e6a7e577b5fe6286de80d44d50f
vihwan <vihwan@ajou.ac.kr>
2019-07-23 17:29:00 +0900

    Refactor : Undo Statement And Create updatePlusQuality method

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -3,7 +3,6 @@ package ac.kr.ajou.dirt;
 class DirtySample {
     Item[] items;
 
-
     public DirtySample(Item[] items) {
         this.items = items;
     }
@@ -11,8 +10,10 @@ class DirtySample {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
 
+
             //First IF
-            if (isNotAged_brieAndBackStage(items[i])) {
+            if (!items[i].name.equals("Aged Brie")
+                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
@@ -20,18 +21,18 @@ class DirtySample {
                 }
             } else {
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                    updatePlusQuality(i);
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                                updatePlusQuality(i);
                             }
                         }
 
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                                updatePlusQuality(i);
                             }
                         }
                     }
@@ -48,24 +49,26 @@ class DirtySample {
 
             //Third IF
             if (items[i].sellIn < 0) {
-                if (isNotAged_brieAndBackStage(items[i])) {
-                    if (items[i].quality > 0) {
-                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                            items[i].quality = items[i].quality - 1;
+                if (!items[i].name.equals("Aged Brie")) {
+                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (items[i].quality > 0) {
+                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                                items[i].quality = items[i].quality - 1;
+                            }
                         }
+                    } else {
+                        items[i].quality = 0;
                     }
-                }
-                items[i].quality = 0;
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                } else {
+                    if (items[i].quality < 50) {
+                        updatePlusQuality(i);
+                    }
                 }
             }
         }
     }
 
-
-    private boolean isNotAged_brieAndBackStage(Item item) {
-        return !item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    private void updatePlusQuality(int i) {
+        items[i].quality = items[i].quality + 1;
     }
 }
