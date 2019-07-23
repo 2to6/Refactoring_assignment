b03067c9919fd44e2ae89e8abbb0a47257bcc8dd
vihwan <vihwan@ajou.ac.kr>
2019-07-23 16:36:50 +0900

    Refactor : Extract to 'isNotAged_brieAndBackStage' Method

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -3,14 +3,16 @@ package ac.kr.ajou.dirt;
 class DirtySample {
     Item[] items;
 
+
     public DirtySample(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+
+            //First IF
+            if (isNotAged_brieAndBackStage(items[i])) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
@@ -36,27 +38,34 @@ class DirtySample {
                 }
             }
 
+
+            //Second IF
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
+
+
+            //Third IF
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+                if (isNotAged_brieAndBackStage(items[i])) {
+                    if (items[i].quality > 0) {
+                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            items[i].quality = items[i].quality - 1;
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                }
+                items[i].quality = 0;
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
                 }
             }
         }
+    }
+
+
+    private boolean isNotAged_brieAndBackStage(Item item) {
+        return !item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
 }
